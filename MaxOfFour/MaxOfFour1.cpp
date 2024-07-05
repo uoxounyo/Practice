91ec06cdd14ce1362130ac49e5823831f5fae1f1
塩口晴華 <kmm2459326@stu.o-hara.ac.jp>
2024-07-05 14:01:37 +0900

    プロジェクトを登録

---
+++ MaxOfFour/MaxOfFour1.cpp
@@ -0,0 +1,33 @@
+//==============================================
+//  4値の最大値
+//==============================================
+#include <stdio.h>
+#include <stdlib.h>  // srand(),rand()
+#include <time.h>    // time()
+// 関数プロトタイプ
+int MaxOfFour(int a, int b, int c, int d);
+
+int main()
+{
+	srand(time(nullptr));
+	int a = rand() % 100;
+	int b = rand() % 100;
+	int c = rand() % 100;
+	int d = rand() % 100;
+	int ans = MaxOfFour(a, b, c, d);
+	printf("MaxOfFour(%d,%d,%d,%d) => %d\n", a, b, c, d, ans);
+	return 0;
+}
+int MaxOfFour(int a, int b, int c, int d)
+{
+	int max = a;
+
+	if (b > max)
+		max = b;
+	if (c > max)
+		max = c;
+	if (d > max)
+		max = d;
+
+	return max;
+}
